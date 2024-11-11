b62c2dfca14794d5d99fb454adb9f072250e563a
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2024-11-11 12:58:18 -0400

    0f

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,26 +1,21 @@
-int _putchar(char c);
+int _isupper(int c);
 
-void print_alphabet(void);
+int _isdigit(int c);
 
-void print_alphabet_x10(void);
+int mul(int a, int b);
 
-int _islower(int c);
+void print_numbers(void);
 
-int _isalpha(int c);
+void print_most_numbers(void);
 
-int print_sign(int n);
+void more_numbers(void);
 
-int _abs(int);
+void print_line(int n);
 
-int print_last_digit(int);
+void print_diagonal(int n);
 
-void jack_baver(void);
+void print_square(int size);
 
-void times_table(void);
+void print_triangle(int size);
 
-int add (int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
+void print_number(int n);
