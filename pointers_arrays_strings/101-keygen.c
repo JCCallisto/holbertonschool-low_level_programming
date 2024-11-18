b0d2c15f9686b2cdf0ac15f15369d89a49180877
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2024-11-18 15:15:01 -0400

    11m

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,31 +3,33 @@
 #include <time.h>
 
 /**
- * main - Generates random valid passwords for the
- * program 101-crackme.
- * Return: Always 0.
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
+        int pass[100];
+        int i, sum, n;
 
-	char password[84];
-	int i, sum, diff;
+        sum = 0;
 
-	srand(time(NULL));
+        srand(time(NULL));
 
-	for (i = 0, sum = 0 < 2772 - 122; i++;)
-	{
-	password[i] = rand() % 94 + 33;
-	sum += password[i];
-	}
+        for (i = 0; i < 100; i++)
+        {
+                pass[i] = rand() % 78;
+                sum += (pass[i] + '0');
+                putchar(pass[i] + '0');
+                if ((2772 - sum) - '0' < 78)
+                {
+                        n = 2772 - sum - '0';
+                        sum += n;
+                        putchar(n + '0');
+                        break;
+                }
+        }
 
-	diff = sum - 2772 + 122;
-	password[i] = diff;
-
-	printf("%s", password);
-
-	return(0);
-
+        return (0);
 }
