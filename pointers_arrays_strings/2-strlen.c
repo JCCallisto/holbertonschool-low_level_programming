4cc3af8a4d7aa783db0e23f7ea6451c2007e7a32
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2025-03-24 11:50:03 -0400

    Pointers, Arrays and Strings

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * _strlen - Returns the length of a string
- * @s: The string to get the length of
- * Return: Length of @s.
+ *  _strlen - Returns the length of a string.
+ *  @str: The string to get the length of
+ *  Return: The length of @str.
  */
 
-size_t _strlen(const char *s)
+size_t _strlen(const char *str)
 
 {
 	size_t length = 0;
 
-	while (*s++);
+	while (*str++)
 	length++;
 	return (length);
 }
