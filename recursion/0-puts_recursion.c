022dd670f9980a5d0f2fd02618e8010738062ef2
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2025-04-21 09:03:41 -0400

    Fixing simple error

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * _puts_recursion - Prints a string followed by a new line.
@@ -11,11 +11,11 @@
 void _puts_recursion(char *s)
 
 {
-	if (*s == 0)
+	if (*s == '\0')
 	{
-		putchar ('\n');
+		write(1, "\n", 1);
 		return;
 	}
-	putchar(*s);
+	write(1, s, 1);
 	_puts_recursion(s + 1);
 }
--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -15,8 +15,6 @@ void _print_rev_recursion(char *s)
 		return;
 	}
 
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	putchar(*s);
+	_print_rev_recursion(s + 1);
+	write(1, s, 1);
 }
