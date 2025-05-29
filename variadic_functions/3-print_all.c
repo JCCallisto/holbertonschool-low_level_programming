23053f916b71a1b0a21a922d9afa7bb124bf1e57
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2025-05-29 14:10:59 -0400

    Variadic Functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -62,7 +62,8 @@ void print_all(const char * const format, ...)
 {
 	const char *ptr;
 	va_list list;
-	struct {
+	struct
+	{
 		char spec;
 		void (*f)(va_list);
 	} key[4] = { {'c', printf_char}, {'i', printf_int},
