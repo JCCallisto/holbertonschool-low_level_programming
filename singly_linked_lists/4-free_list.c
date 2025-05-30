d37a1e46ea78b1cf112a8924ca44364b9506ac74
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2025-05-30 18:56:48 -0400

    Singly linked lists

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -12,10 +12,10 @@ void free_list(list_t *head)
 	list_t *temp;
 	
 	while (head != NULL)
-    {
-	    temp = head;
-	    head = head->next;
-	    free(temp->str);
-	    free(temp);
-    }
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
