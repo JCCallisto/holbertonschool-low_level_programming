efd169fff7faead5d5b6b6bbd15c19431e21b8de
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2025-05-30 18:37:56 -0400

    Singly linked lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -14,6 +14,16 @@
  * Description: Singly linked list node structure.
  */
 
+struct list_s
+
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+};
+
+typedef struct list_s list_t;
+
 int _putchar(char c);
 
 size_t print_list(const list_t *h);
