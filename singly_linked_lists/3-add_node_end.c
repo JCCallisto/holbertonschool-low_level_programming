021ff30bab402a8caea849f45a96a55f698e5a20
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2025-05-30 18:50:37 -0400

    Singly linked lists

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,25 +15,25 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *current;
 	char *dup_str;
 	unsigned int len = 0;
-	
+
 	if (str == NULL)
 	return (NULL);
-	
+
 	while (str[len])
 	len++;
-	
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	return (NULL);
-	
+
 	dup_str = strdup(str);
 	if (dup_str == NULL)
-	
+
 	{
 		free(new_node);
 		return (NULL);
 	}
-	
+
 	new_node->str = dup_str;
 	new_node->len = len;
 	new_node->next = NULL;
@@ -45,8 +45,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	current = *head;
 	while (current->next != NULL)
 	current = current->next;
-	
+
 	current->next = new_node;
-	
+
 	return (new_node);
 }
