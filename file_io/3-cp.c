9c0f07b3b7d31b4c08cfce8ccc8b07e0d0a84a30
JCCallisto <jcchronokeeper@gmail.com>
2025-09-20 14:31:51 -0400

    Still Fixing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -49,34 +49,34 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			if (bytes_written == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				close_fd(fd_from);
+				close_fd(fd_to);
+				exit(99);
+			}
+			else
+			{
+				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+				close_fd(fd_from);
+				close_fd(fd_to);
+				exit(98);
+			}
+		}
+	}
+
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close_fd(fd_from);
 		close_fd(fd_to);
 		exit(98);
-	}
-
-	while (bytes_read > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_fd(fd_from);
-			close_fd(fd_to);
-			exit(99);
-		}
-
-		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close_fd(fd_from);
-			close_fd(fd_to);
-			exit(98);
-		}
 	}
 
 	close_fd(fd_from);
