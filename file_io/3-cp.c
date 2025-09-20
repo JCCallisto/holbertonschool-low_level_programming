79e5a14c1b546db116761764ca49d616759ccd5e
JCCallisto <jcchronokeeper@gmail.com>
2025-09-20 13:17:52 -0400

    Copies file contents

---
+++ file_io/3-cp.c
@@ -0,0 +1,93 @@
+#include "main.h"
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * close_file - closes file descriptor and handles error
+ * @fd: file descriptor to close
+ */
+
+void close_file(int fd)
+
+{
+	int result;
+
+	result = close(fd);
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, exit codes 97, 98, 99, or 100 on various errors
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	/* Check correct number of arguments */
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	/* Open source file for reading */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	/* Create/open destination file for writing */
+	/* Permissions: rw-rw-r-- (0664) */
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_file(fd_from);
+		exit(99);
+	}
+
+	/* Copy content from source to destination */
+	do {
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close_file(fd_from);
+			close_file(fd_to);
+			exit(98);
+		}
+
+		if (bytes_read > 0)
+		{
+			bytes_written = write(fd_to, buffer, bytes_read);
+			if (bytes_written == -1 || bytes_written != bytes_read)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				close_file(fd_from);
+				close_file(fd_to);
+				exit(99);
+			}
+		}
+	} while (bytes_read > 0);
+
+	/* Close both files */
+	close_file(fd_from);
+	close_file(fd_to);
+
+	return (0);
+}
