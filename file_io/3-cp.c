b7fbeaa887430433859178aa60d8315946381998
JCCallisto <jcchronokeeper@gmail.com>
2025-09-20 13:48:06 -0400

    Fixing read value

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,9 +7,7 @@
  * close_file - closes file descriptor and handles error
  * @fd: file descriptor to close
  */
-
 void close_file(int fd)
-
 {
 	int result;
 
@@ -28,9 +26,7 @@ void close_file(int fd)
  *
  * Return: 0 on success, exit codes 97, 98, 99, or 100 on various errors
  */
-
 int main(int argc, char *argv[])
-
 {
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
@@ -62,19 +58,28 @@ int main(int argc, char *argv[])
 	}
 
 	/* Copy content from source to destination */
-	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-	if (bytes_read == -1)
+	while (1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close_file(fd_from);
-		close_file(fd_to);
-		exit(98);
-	}
-	
-	while (bytes_read > 0)
-	{
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+		
+		/* If read returns -1, it's a read error */
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close_file(fd_from);
+			close_file(fd_to);
+			exit(98);
+		}
+		
+		/* If read returns 0, we've reached end of file */
+		if (bytes_read == 0)
+			break;
+		
+		/* Try to write what we read */
 		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
+		
+		/* If we successfully read but can't write, it's a write error */
+		if (bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close_file(fd_from);
@@ -82,13 +87,13 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 		
-		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
+		/* If we wrote fewer bytes than we read, it's also a write error */
+		if (bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close_file(fd_from);
 			close_file(fd_to);
-			exit(98);
+			exit(99);
 		}
 	}
 
