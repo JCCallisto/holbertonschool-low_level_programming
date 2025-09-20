be9ba0bef12c8a41cee935dd95503b4087ef7963
JCCallisto <jcchronokeeper@gmail.com>
2025-09-20 14:25:19 -0400

    Fixing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,94 +4,73 @@
 #define BUFFER_SIZE 1024
 
 /**
+ * close_fd - closes a file descriptor and exits with code 100 if it fails
+ * @fd: file descriptor to close
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: 0 on success, or exit with specific codes on failure
  */
-
 int main(int argc, char *argv[])
-
 {
 	int fd_from, fd_to, bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	/* Open source file for reading */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	/* Create/open destination file for writing with permissions rw-rw-r-- */
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		if (close(fd_from) == -1)
-		{
-			dprintf(2, "Error: Can't close fd %d\n", fd_from);
-			exit(100);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_fd(fd_from);
 		exit(99);
 	}
 
-	/* Copy file contents */
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			if (close(fd_from) == -1)
-			{
-				dprintf(2, "Error: Can't close fd %d\n", fd_from);
-				exit(100);
-			}
-			if (close(fd_to) == -1)
-			{
-				dprintf(2, "Error: Can't close fd %d\n", fd_to);
-				exit(100);
-			}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
 			exit(99);
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		if (close(fd_from) == -1)
-		{
-			dprintf(2, "Error: Can't close fd %d\n", fd_from);
-			exit(100);
-		}
-		if (close(fd_to) == -1)
-		{
-			dprintf(2, "Error: Can't close fd %d\n", fd_to);
-			exit(100);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
 		exit(98);
 	}
 
-	/* Close file descriptors */
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	close_fd(fd_from);
+	close_fd(fd_to);
 
 	return (0);
 }
